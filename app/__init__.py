bddb9c96744a62e6e001d57bc47144de16b84c52
JBeamer92 <jared.c.beamer@gmail.com>
2020-11-20 00:25:34 -0500

    Calling an audible - I'm going to use FastAPI for my backend and Vue.js for the front-end. This commit contains FastAPI basic shell

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from .blueprints.page import page
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(page)
-    return app
---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return "I'm Ready!"
+
+@app.get("/apps")
+async def get_apps():
+    return [{
+        "company": "Anthem",
+        "position": "Developer",
+    }, {
+        "company": "Kindred",
+        "position": "Programmer Analyst",
+    }]
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
