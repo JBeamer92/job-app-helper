b1e8052b3ca529aac96294ba07c058fca8c179fa
JBeamer92 <jared.c.beamer@gmail.com>
2020-12-01 21:02:49 -0500

    removed unused method

--- app/main.py
+++ app/main.py
@@ -132,18 +132,9 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_users(skip=skip, limit=limit, db=db)
 
 
-@app.get("/users/me")
+@app.get("/users/me", response_model=schemas.User)
 def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
     return current_user
-
-
-# I may not need this method... Removing for now
-# @app.get('/users/{user_id}', response_model=schemas.User)
-# def get_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db=db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found.")
-#     return db_user
 
 
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
@@ -156,7 +147,7 @@ def create_item_for_user(
 # TOKEN MANAGEMENT
 
 
-@app.post('/token')
+@app.post('/token', response_model=schemas.Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     db_user = authenticate_user(db=db, email=form_data.username, password=form_data.password)
     if not db_user:
@@ -201,12 +192,6 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
-
-
-def fake_decode_token(token, db):
-    # because right now, the token is the username (email), we can just pass the token in to get the user
-    user = crud.get_user_by_email(db=db, email=token)
-    return user
 
 
 if __name__ == '__main__':
